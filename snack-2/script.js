55cd5e9c3a09d9f33427383a3022313fbaecb330
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-17 12:39:34 +0100

    feat: snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,9 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+// estrai tutti i nomi (crea un nuovo array - MAP)
+const names = people.map(people => people.name);
+
+// stampa in console tutti i nomi
+console.log(names.join( ));
